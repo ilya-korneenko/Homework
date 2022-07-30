80c9423e92e47fb59c1a5a3932f1a2aeff8c1cf1
ilya-korneenko <korneenko.2001@mail.ru>
2022-07-30 08:39:07 +0300

    created CalculatorWithMemory and CalculatorWithMemoryMain(from Task12)

---
+++ HomeWork/src/HomeWork3/calcs/additional/CalculatorWithMemory.java
@@ -0,0 +1,78 @@
+package HomeWork3.calcs.additional;
+
+import HomeWork3.calcs.api.ICalculator;
+
+import java.util.Scanner;
+
+public class CalculatorWithMemory {
+    ICalculator iCalculator;
+    private double memoryTemp;
+
+    public CalculatorWithMemory(ICalculator iCalculator) {
+        this.iCalculator = iCalculator;
+    }
+
+    public double division(double divisible, double divisor) {
+        memoryTemp = iCalculator.division(divisible, divisor);
+        memoryValue();
+        return iCalculator.division(divisible, divisor);
+    }
+
+    public double multiplier(double first_num, double second_num) {
+        memoryTemp = iCalculator.multiplier(first_num, second_num);
+        memoryValue();
+        return iCalculator.multiplier(first_num, second_num);
+    }
+
+    public double sum(double first_summand, double second_summand) {
+        memoryTemp = iCalculator.sum(first_summand,second_summand);
+        memoryValue();
+        return iCalculator.sum(first_summand, second_summand);
+    }
+
+    public double difference(double reduced, double subtractible) {
+        memoryTemp = iCalculator.difference(reduced,subtractible);
+        memoryValue();
+        return iCalculator.difference(reduced, subtractible);
+    }
+
+    public double pow(double number, int degree) {
+        memoryTemp = iCalculator.pow(number,degree);
+        memoryValue();
+        return iCalculator.pow(number, degree);
+    }
+
+    public double module(double number) {
+        memoryTemp = iCalculator.module(number);
+        memoryValue();
+        return iCalculator.module(number);
+    }
+
+    public double mySqrt(double number) {
+        memoryTemp = iCalculator.mySqrt(number);
+        memoryValue();
+        return iCalculator.mySqrt(number);
+    }
+
+    private void memoryValue() {
+        double temp = getMemoryTemp();
+        if (choice()) {
+            System.out.println(temp);
+        }
+    }
+
+    public void setMemoryTemp(double memoryTemp) {
+        this.memoryTemp = memoryTemp;
+    }
+
+    public double getMemoryTemp() {
+        return memoryTemp;
+    }
+
+    private boolean choice() {
+        Scanner console = new Scanner(System.in);
+        System.out.print("Вывести ли значение из памяти:\n_______________________________\n");
+        System.out.print("да - true\n_________\nнет - false\n__________\n");
+        return console.nextBoolean();
+    }
+}
---
+++ HomeWork/src/HomeWork3/runners/CalculatorWithMemoryMain.java
@@ -0,0 +1,16 @@
+package HomeWork3.runners;
+
+import HomeWork3.calcs.additional.CalculatorWithMemory;
+import HomeWork3.calcs.api.ICalculator;
+import HomeWork3.calcs.simple.CalculatorWithOperator;
+
+public class CalculatorWithMemoryMain {
+    public static void main(String[] args) {
+        CalculatorWithMemory calculator = new CalculatorWithMemory(new CalculatorWithOperator());
+        double firstAction = calculator.multiplier(15, 7);
+        double secondAction = calculator.division(28, 5);
+        double thirdAction = calculator.pow(secondAction, 2);
+        double fourthAction = calculator.sum(4.1, firstAction);
+        double fifthAction = calculator.sum(fourthAction, thirdAction);
+    }
+}
